d9d01724d911f459545474c0a5997398b449528b
Vilaca <khallil.vilaca@gmail.com>
2023-02-13 15:36:53 -0300

    remove commit

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,3 @@ app.use(express.json());
 app.use(router);
 
 app.listen(process.env.APP_PORT, () => console.log('Server is running'));
-
-
-// import database from './database'
-
-// database.initialize().then(() => {
-//     console.log('Connected successfully')
-
-//     const app = express()
-
-//     app.use(express.json())
-    
-//     app.get('/', (req, res) => {
-//         return res.json('hello word')
-//     })
-
-//     return app.listen(process.env.APP_PORT)
-// })
