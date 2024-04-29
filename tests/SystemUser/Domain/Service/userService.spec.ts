78fd75b08ce4a41d38c7ab4720ea52390bd9bae7
vilaca <khallil.vilaca@gmail.com>
2024-04-28 21:38:42 -0300

    feat: created test to it should not be possible to register with the same username twice

--- tests/SystemUser/Domain/Service/userService.spec.ts
+++ tests/SystemUser/Domain/Service/userService.spec.ts
@@ -2,6 +2,7 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import { UserService } from '../../../../src/SystemUser/Domain/Service/UserService';
 import { User } from '../../../../src/SystemUser/Domain/Entity/User';
 import { UserId } from '../../../../src/SystemUser/Domain/Entity/UserId';
+import { UnprocessableEntity } from '../../../../Kernel/http/UnprocessableEntity';
 
 let userService: UserService;
 
@@ -10,13 +11,25 @@ describe('User userService', () => {
         userService = new UserService();
     })
 
+    let user: User;
     it('should be able to create a user', async () => {
-        const user = await userService.save(new User(
+        user = new User(
             new UserId(null),
             'Khallil Vilaca Salinas',
-            'Lucas',
-            '1234'
-        ));
+            'Lucas1',
+            '123'
+        );
 
-    })
-})
+        const newUser = await userService.save(user);
+
+        expect(newUser).toBeInstanceOf(User);
+        expect(newUser.getUsername).toBe(user.getUsername);
+        expect(newUser.getPassword).not.toBe(user.getPassword)
+    });
+
+    it('it should not be possible to register with the same username twice', async () => {
+        await expect( async () => {
+            await userService.save(user);
+        }).rejects.toBeInstanceOf(UnprocessableEntity)
+    });
+});
